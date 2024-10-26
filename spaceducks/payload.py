9933c772919a0eec2eb382ec17832774cb27db50
Aman <IA.AmanAnas@gmail.com>
2024-10-26 15:25:55 -0400

    Move sensor state to separate file

--- spaceducks/payload.py
+++ spaceducks/payload.py
@@ -10,10 +10,9 @@ import board
 from adafruit_bno055 import BNO055_I2C
 from adafruit_bmp280 import Adafruit_BMP280_I2C
 
-# Data storage and serialization
-import msgspec
 
 from .radio_interface import RFInterface
+from .state import SensorState, FlightStats
 
 
 class LaunchState(Enum):
@@ -21,43 +20,6 @@ class LaunchState(Enum):
     ARMED = auto()
     LANDED = auto()
     RECOVER = auto()
-
-
-class SensorState(msgspec.Struct):
-    altitude: float = 0.0
-    temperature: float = 0.0
-    orientation: tuple[float, float, float] = (0.0, 0.0, 0.0)
-
-    # For measuring forces on STEMnauts
-    acceleration: tuple[float, float, float] = (0.0, 0.0, 0.0)
-    # For detecting launch
-    linear_accel: tuple[float, float, float] = (0.0, 0.0, 0.0)
-
-    def __str__(self) -> str:
-        return (
-            f"altitude: {self.altitude:5.2f}, "
-            f"temperature: {self.temperature:5.2f},"
-            f"orientation: {[f'{val:5.2f}' for val in self.orientation ]}, "
-            f"acceleration: {[f'{val:5.2f}' for val in self.acceleration ]}, "
-            f"linear_acceleration: {[f'{val:5.2f}' for val in self.linear_accel ]}"
-        )
-
-
-class FlightStats(msgspec.Struct):
-    max_acceleration: float = 0.0
-    max_temperature: float = 0.0
-    max_altitude: float = 0.0
-    survivability_rating: float = 0.0
-
-    def __str__(self) -> str:
-        """This string will be used for text-to-speech"""
-
-        return (
-            f"Maximum Acceleration: {self.max_acceleration}. "
-            f"Maximum Temperature: {self.max_temperature}. "
-            f"Maximum Altitude: {self.max_altitude}. "
-            f"STEMnaut Survivability: {self.survivability_rating * 100.0} percent. "
-        )
 
 
 class PayloadSystem:
@@ -157,6 +119,7 @@ class PayloadSystem:
                 self.last_log_time = time.time()
 
     def update_stats(self):
+
         if self.data.altitude > self.stats.max_altitude:
             self.stats.max_altitude = self.data.altitude
 
--- spaceducks/radio_interface.py
+++ spaceducks/radio_interface.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .payload import FlightStats
+    from .state import FlightStats
 
 import serial
 import sounddevice as sd
@@ -18,22 +18,28 @@ class RadioPTT:
     """Helper Class to trigger and un-trigger PTT (Push-to-talk)"""
 
     def __init__(self, ptt_port: str) -> None:
-        self.ptt = serial.Serial(ptt_port)
+        # self.ptt = serial.Serial(ptt_port)
+        ...
 
     def __enter__(self) -> RadioPTT:
-        self.ptt.rts = True
-        self.ptt.dtr = False
+        # self.ptt.rts = True
+        # self.ptt.dtr = False
 
-        time.sleep(1)  # Give it time to start transmiting
+        # time.sleep(1)  # Give it time to start transmiting
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.ptt.rts = False
-        self.ptt.dtr = True
+        # self.ptt.rts = False
+        # self.ptt.dtr = True
+        ...
 
 
 class RFInterface:
+
+    # Name of the file to save our audio as
     AUDIO_FILE_NAME = "sensor_audio.wav"
+
+    # Name of the audio device to play. Will try and match a substring
     AUDIO_DEVICE_SUBSTR = "All"
 
     def __init__(self, callsign: str, ptt_port: str):
@@ -42,16 +48,15 @@ class RFInterface:
 
     def transmit_data(self, data: FlightStats):
         logging.info("Generating TTS...")
-        
-        cmd = (
-            "espeak \""
-            f"This is {self.callsign} for Student Launch. "
-            f"{str(data)}"
-            f"This is {self.callsign}. \" -w "
-            f"{self.AUDIO_FILE_NAME}"
-        )
-        subprocess.call(cmd, shell=True)
 
+        cmd = (
+            "espeak",
+            "-w",
+            f"{self.AUDIO_FILE_NAME}",
+            f'"This is {self.callsign}-1 for Student Launch. {str(data)} This is {self.callsign}-1. "',
+        )
+
+        subprocess.call(cmd, shell=True)
         logging.info("Reading audio file...")
         audio, samplerate = sf.read(self.AUDIO_FILE_NAME)
 
@@ -64,3 +69,9 @@ class RFInterface:
 
         # 'with' block automatically triggers and releases PTT
         logging.info("Done speaking.")
+
+
+# For testing the radio interface and PTT
+if __name__ == "__main__":
+    interface = RFInterface("hello", "")
+    interface.transmit_data(None)
