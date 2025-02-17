7db300bc0990ff526edae44ac651b44ea50c8cbc
NateOrlowski06 <NateOrlowski@protonmail.com>
2025-02-17 18:31:20 -0500

    Changed velocity window to ensure accurate landing velocity

--- payload/constants.py
+++ payload/constants.py
@@ -113,7 +113,7 @@ ALTITUDE_DEADBAND_METERS = 0.05
 """The deadband for the altitude in meters. This is used to prevent small fluctuations in altitude
 from being considered as a change in altitude."""
 
-VELOCITY_FROM_ALTITUDE_WINDOW_SIZE = 10
+VELOCITY_FROM_ALTITUDE_WINDOW_SIZE = 50
 
 # -------------------------------------------------------
 # Transmitter Configuration
