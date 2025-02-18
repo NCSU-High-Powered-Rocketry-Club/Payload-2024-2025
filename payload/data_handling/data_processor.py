c6d99d58081b86a6f689feabc80181ba5d88b11d
Will Sanderson <wlsander@ncsu.edu>
2025-02-18 17:34:49 -0500

    removed rotated accel

--- payload/data_handling/data_processor.py
+++ payload/data_handling/data_processor.py
@@ -23,19 +23,15 @@ class DataProcessor:
 
     __slots__ = (
         "_current_altitude",
-        "_current_orientation_quaternions",
         "_data_packet",
         "_filter",
         "_initial_altitude",
         "_last_data_packet",
         "_last_velocity_calculation_packet",
         "_max_altitude",
-        "_max_velocity_from_acceleration",
         "_previous_vertical_velocity",
-        "_rotated_acceleration",
         "_time_difference",
-        "_velocity_from_acceleration",
-        "_velocity_from_altitude",
+        "_vertical_velocity",
     )
 
     def __init__(self):
@@ -48,18 +44,15 @@ class DataProcessor:
         maximum velocity of the rocket.
         """
         self._max_altitude: np.float64 = np.float64(0.0)
-        self._velocity_from_acceleration: np.float64 = np.float64(0.0)
-        self._max_velocity_from_acceleration: np.float64 = np.float64(0.0)
         self._initial_altitude: np.float64 | None = None
         self._current_altitude: np.float64 = np.float64(0.0)
         self._last_data_packet: IMUDataPacket | None = None
-        self._current_orientation_quaternions: R | None = None
-        self._rotated_acceleration: np.float64 = np.float64(0.0)
         self._data_packet: IMUDataPacket | None = None
         self._time_difference: np.float64 = np.float64(0.0)
         self._previous_vertical_velocity: np.float64 = np.float64(0.0)
-        self._velocity_from_altitude: np.float64 = np.float64(0.0)
+        self._vertical_velocity: np.float64 = np.float64(0.0)
         self._last_velocity_calculation_packet: IMUDataPacket | None = None
+        self._filter = ahrs.filters.Davenport(magnetic_dip=62, weights=[3,1])
 
     @property
     def max_altitude(self) -> float:
@@ -78,28 +71,20 @@ class DataProcessor:
         return float(self._current_altitude)
 
     @property
-    def velocity_from_altitude(self) -> float:
+    def vertical_velocity(self) -> float:
         """
         Returns the vertical velocity of the rocket in m/s. Calculated by differentiating the
         altitude.
         """
-        return float(self._velocity_from_altitude)
+        return float(self._vertical_velocity)
 
     @property
-    def velocity_from_acceleration(self) -> float:
-        """The current vertical velocity of the rocket in m/s. Calculated by integrating the
-        compensated acceleration."""
-        return float(self._velocity_from_acceleration)
-
-    @property
-    def max_velocity_from_acceleration(self) -> float:
-        """The maximum vertical velocity the rocket has attained during the flight, in m/s."""
-        return float(self._max_velocity_from_acceleration)
-
-    @property
-    def vertical_acceleration(self) -> float:
-        """The vertical acceleration of the rocket in m/s^2."""
-        return float(self._rotated_acceleration)
+    def max_vertical_velocity(self) -> float:
+        """
+        Returns the highest vertical velocity attained by the rocket for the entire flight
+        so far, in meters per second.
+        """
+        return float(self._max_velocity)
 
     @property
     def current_timestamp(self) -> int:
@@ -109,16 +94,11 @@ class DataProcessor:
         except AttributeError:  # If we don't have a last data packet
             return 0
 
-    @property
-    def roll_pitch_yaw(self) -> tuple[np.float64, np.float64, np.float64]:
-        """The roll pitch and yaw of the rocket, in degrees."""
-        return tuple(R.from_quat(self._current_orientation_quaternions, scalar_first=True).as_euler("xyz", degrees=True))
 
     def update(self, data_packet: IMUDataPacket) -> None:
         """
-        Updates the data points to process. This will recompute all information and handle math
-        related to orientation (quaternions/pitch roll yaw) such as altitude, velocity,
-        acceleration, and crew survivability.
+        Updates the data points to process. This will recompute all calculations for altitude,
+        velocity, acceleration, and crew survivability.
         :param data_packet: An IMUDataPacket object to process
         """
         # If we don't have a data packet, return early
@@ -129,10 +109,8 @@ class DataProcessor:
 
         # If we don't have a last data point, we can't calculate the time differences needed
         # for velocity calculation:
-        if self._last_data_packet is None and data_packet.magneticFieldX:
+        if self._last_data_packet is None:
             self._first_update()
-        elif self._last_data_packet is None:
-            return
 
         self._time_difference = np.float64(
             convert_milliseconds_to_seconds(
@@ -140,15 +118,11 @@ class DataProcessor:
             )
         )
 
-        self._rotated_acceleration = self._calculate_rotated_acceleration()
-        self._velocity_from_acceleration = self._calculate_velocity_from_acceleration()
-        self._velocity_from_altitude = self._calculate_velocity_from_altitude()
-        self._max_velocity_from_acceleration = max(
-            self._velocity_from_acceleration, self._max_velocity_from_acceleration
-        )
+        self._vertical_velocity = self._calculate_velocity_from_altitude()
 
         self._current_altitude = self._calculate_current_altitude()
         self._max_altitude = max(self._current_altitude, self._max_altitude)
+        self._max_velocity = max(self._vertical_velocity, self._max_velocity)
 
         # Store the last data point for the next update
         self._last_data_packet = data_packet
@@ -161,15 +135,10 @@ class DataProcessor:
         """
         return ProcessorDataPacket(
             current_altitude=self._current_altitude,
-            velocity_from_acceleration=self._velocity_from_acceleration,
-            velocity_from_altitude=self._velocity_from_altitude,
-            vertical_acceleration=self._rotated_acceleration,
+            vertical_velocity=self._vertical_velocity,
             time_since_last_data_packet=self._time_difference,
             maximum_altitude=np.float64(self.max_altitude),
-            maximum_velocity=np.float64(self.max_velocity_from_acceleration),
-            roll=self.roll_pitch_yaw[0],
-            pitch=self.roll_pitch_yaw[1],
-            yaw=self.roll_pitch_yaw[2],
+            maximum_velocity=np.float64(self.max_vertical_velocity),
             # TODO: Implement these
             crew_survivability=0.0,
             landing_velocity=0.0,
@@ -178,7 +147,7 @@ class DataProcessor:
     def _first_update(self) -> None:
         """
         Sets up the initial values for the data processor. This includes setting the initial
-        altitude, and the initial orientation of the rocket. This should
+        altitude. This should
         only be called once, when the first data packets are passed in.
         """
         # Setting last data packet as this packet makes it so that the time diff
@@ -188,24 +157,6 @@ class DataProcessor:
         # This is us getting the rocket's initial altitude from the first data packets
         self._initial_altitude = self._data_packet.pressureAlt
 
-        acc = np.array(
-            [
-                self._data_packet.estCompensatedAccelX,
-                self._data_packet.estCompensatedAccelY,
-                self._data_packet.estCompensatedAccelZ,
-            ]
-        )
-        mag = np.array(
-            [
-                self._data_packet.magneticFieldX,
-                self._data_packet.magneticFieldY,
-                self._data_packet.magneticFieldZ,
-            ]
-        )
-
-        self._filter = ahrs.filters.Davenport(acc=acc, mag=mag, magnetic_dip=62, weights=[5,1])
-        self._current_orientation_quaternions = self._filter.estimate(acc=acc, mag=mag)
-
     def _calculate_current_altitude(self) -> np.float64:
         """
         Calculates the current altitude, by zeroing out the initial altitude.
@@ -213,61 +164,6 @@ class DataProcessor:
         """
         # Get the pressure altitude from the data points and zero out the initial altitude
         return self._data_packet.pressureAlt - self._initial_altitude
-
-    def _calculate_rotated_acceleration(self) -> np.float64:
-        """
-        Calculates the rotated vertical acceleration.
-
-        :return: float containing the vertical acceleration
-        """
-        acc = np.array(
-            [
-                self._data_packet.estCompensatedAccelX,
-                self._data_packet.estCompensatedAccelY,
-                self._data_packet.estCompensatedAccelZ,
-            ]
-        )
-        mag = np.array(
-            [
-                self._data_packet.magneticFieldX,
-                self._data_packet.magneticFieldY,
-                self._data_packet.magneticFieldZ,
-            ]
-        )
-
-        # update quaternion heading with ahrs, use MARG if magnetometer is available
-        if all(mag_data_point is not None for mag_data_point in mag):
-            self._current_orientation_quaternions = self._filter.estimate(acc=acc, mag=mag)
-
-        # Rotate the acceleration vector using the orientation
-        rotated_accel = R.from_quat(self._current_orientation_quaternions, scalar_first=True).apply([acc[0], acc[1], acc[2]])
-
-        # Vertical acceleration will always be the 3rd element of the rotated vector,
-        # regardless of orientation.
-        return rotated_accel[2]
-
-    def _calculate_velocity_from_acceleration(self) -> np.float64:
-        """
-        Calculates the velocity of the rocket based on the rotated compensated acceleration.
-        Integrates that acceleration to get the velocity.
-        :return: A numpy array of the velocity of the rocket at each data packet
-        """
-        # Gets the vertical acceleration from the rotated vertical acceleration. gravity needs to be
-        # subtracted from vertical acceleration, Then deadbanded.
-        vertical_acceleration = deadband(
-            self._rotated_acceleration - GRAVITY_METERS_PER_SECOND_SQUARED,
-            ACCEL_DEADBAND_METERS_PER_SECOND_SQUARED,
-        )
-
-        # Integrate the acceleration to get the velocity
-        vertical_velocity = (
-            self._previous_vertical_velocity + vertical_acceleration * self._time_difference
-        )
-
-        # Store the last calculated velocity vector
-        self._previous_vertical_velocity = vertical_velocity
-
-        return vertical_velocity
 
     def _calculate_velocity_from_altitude(self) -> np.float64:
         """
@@ -298,5 +194,32 @@ class DataProcessor:
             self._last_velocity_calculation_packet = self._data_packet
         else:
             # If the altitude hasn't changed, we use the last velocity
-            velocity = self._velocity_from_altitude
+            velocity = self._vertical_velocity
         return velocity
+
+    def calculate_orientation(self) -> tuple:
+        """
+        Calculates the orientation of the rocket using the magnetometer and accelerometer.
+        :return: a tuple of roll, pitch, and yaw.
+        """
+        acc = np.array(
+            [
+                self._data_packet.estCompensatedAccelX,
+                self._data_packet.estCompensatedAccelY,
+                self._data_packet.estCompensatedAccelZ,
+            ]
+        )
+
+        mag = np.array(
+            [
+                self._data_packet.magneticFieldX,
+                self._data_packet.magneticFieldY,
+                self._data_packet.magneticFieldZ,
+            ]
+        )
+
+        if any(mag_data_point is None for mag_data_point in mag):
+            return None
+
+        orientation = self._filter.estimate(acc=acc, mag=mag)
+        return tuple(R.from_quat(orientation, scalar_first=True).as_euler("xyz", degrees=True))
