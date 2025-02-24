d9b06259a7f584999c0b58d05ce96b88a3d89c50
Graicc <33105645+Graicc@users.noreply.github.com>
2025-02-24 17:20:17 -0500

    Add GPS position reading

--- payload/data_collection_unit/src/bin/main.rs
+++ payload/data_collection_unit/src/bin/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
-use embassy_executor::Spawner;
+use defmt::{debug, info};
+use embassy_executor::{task, Spawner};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::channel::Channel;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::uart::Uart;
+use esp_hal::Async;
 use esp_println as _;
 
 #[panic_handler]
@@ -13,10 +17,58 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+#[derive(Debug)]
+struct GpsPosition {
+    lat: f64,
+    long: f64,
+    alt: f64,
+}
+
+const GPS_CHANNEL: Channel<embassy_sync::blocking_mutex::raw::NoopRawMutex, GpsPosition, 64> =
+    Channel::new();
+
+#[task]
+async fn gps_fetch(uart: Uart<'static, Async>) {
+    let mut parser_buf = [0; 256];
+    let parser_buf = ublox::FixedLinearBuffer::new(&mut parser_buf[..]);
+    let mut parser = ublox::Parser::new(parser_buf);
+
+    loop {
+        let mut buf = [0; 32];
+        match uart.read_exact_async(buf).await {
+            Ok(_) => {
+                let mut it = parser.consume(&buf);
+
+                loop {
+                    let packet = match it.next() {
+                        Some(Ok(packet)) => packet,
+                        Some(Err(_)) => {
+                            continue;
+                        }
+                        None => break,
+                    };
+
+                    let pos = match packet {
+                        ublox::PacketRef::NavPosLlh(nav_pos_llh_ref) => GpsPosition {
+                            lat: nav_pos_llh_ref.lat_degrees(),
+                            long: nav_pos_llh_ref.lon_degrees(),
+                            alt: nav_pos_llh_ref.height_meters(),
+                        },
+                        _ => {
+                            continue;
+                        }
+                    };
+
+                    GPS_CHANNEL.send(pos).await;
+                }
+            }
+            Err(_) => {}
+        }
+    }
+}
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    // generator version: 0.3.0
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -25,13 +77,34 @@ async fn main(spawner: Spawner) {
 
     info!("Embassy initialized!");
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    {
+        // TODO: Check these
+        let baud = 10;
+        let uart_tx = peripherals.GPIO10;
+        let uart_rx = peripherals.GPIO9;
+
+        let uart = esp_hal::uart::Uart::new(
+            peripherals.UART1,
+            esp_hal::uart::Config::default().with_baudrate(baud),
+        )
+        .unwrap()
+        .with_rx(uart_rx)
+        .with_tx(uart_tx)
+        .into_async();
+
+        spawner.spawn(gps_fetch(uart)).unwrap();
+    }
 
     loop {
         info!("Hello world!");
+        loop {
+            match GPS_CHANNEL.try_receive() {
+                Ok(pos) => {
+                    debug!("{:?}", pos);
+                }
+                Err(_) => break,
+            }
+        }
         Timer::after(Duration::from_secs(1)).await;
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
